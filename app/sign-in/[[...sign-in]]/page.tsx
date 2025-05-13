cebbc76f031ab03dca5c30bd85fc57702ad836e3
Vipul Singh <vipulsingh.1404@gmail.com>
2025-05-13 12:05:26 +0530

    authentication added

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <section className="flex justify-center items-center lg:min-h-[40vh] ">
-      <div className="py-12 lg:py-3 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="py-12 lg:py-12 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <BgGradient>
           <SignIn />
         </BgGradient>
