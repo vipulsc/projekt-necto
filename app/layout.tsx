09b32f6746b1e26e2289cb2ea648c869ca73152a
Vipul Singh <vipulsingh.1404@gmail.com>
2025-05-17 23:59:18 +0530

    speed-insight-added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Footer from "@/components/common/footer";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const fontSans = FontSans({
   variable: "--font-sans",
@@ -41,6 +42,7 @@ export default function RootLayout({
           </div>
           <Toaster />
           <Analytics />
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider>
